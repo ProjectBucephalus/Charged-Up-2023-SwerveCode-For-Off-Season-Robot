8380a0bab1c566b5bc3e14eda32ccda50142c85a
ToastedMice <aidan.pawson1@icloud.com>
2023-09-16 19:50:53 +1000

    final swerve code for comp

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,6 +3,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -38,6 +40,10 @@ public class RobotContainer {
     private final Swerve s_Swerve = new Swerve();
     private final Intake s_Intake = new Intake();
 
+    /* Auto */
+    SendableChooser<Command> m_chooser = new SendableChooser<>();
+    private final Command a_ScoreAndDriveFoward = new exampleAuto(s_Swerve, s_Intake);
+    
     
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -51,6 +57,8 @@ public class RobotContainer {
                 () -> driver.getRawAxis(robotCentricButton) > 0.3
             )
         );
+        m_chooser.addOption("ScoreAndDriveForward", a_ScoreAndDriveFoward);
+        SmartDashboard.putData(m_chooser);
         // s_Swerve.setDefaultCommand(
         //     new TeleopSwerve(
         //         s_Swerve, 
@@ -89,6 +97,10 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
-        return new exampleAuto(s_Swerve, s_Intake);
+        return m_chooser.getSelected();
     }
+
+    // public void zeroModules() {
+    //     s_Swerve.
+    // } 
 }
